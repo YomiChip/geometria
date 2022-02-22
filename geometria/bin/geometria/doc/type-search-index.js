145744c0a374f666928e315882463115cd083941
YomiChip <94378713+YomiChip@users.noreply.github.com>
2022-02-22 20:43:23 +0100

    Documentar es de sabios, aunque leerla sea pesado

---
+++ geometria/bin/geometria/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Circulo_AGP"},{"p":"<Unnamed>","l":"FiguraGeometrica_AGP"},{"p":"<Unnamed>","l":"Rectangulo_AGP"},{"p":"<Unnamed>","l":"Triangulo_AGP"}];updateSearchResults();
